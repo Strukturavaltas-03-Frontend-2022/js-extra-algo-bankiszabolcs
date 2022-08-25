31e4babb0d0e3094cb51f7a3d19df2b04778d670
szabi1112 <banki.szabolcs@gmail.com>
2022-08-25 16:16:20 +0200

    5/5

--- solutions/intermediate/number-string-sorter/number-string-sorter.js
+++ solutions/intermediate/number-string-sorter/number-string-sorter.js
@@ -1,5 +1,14 @@
 function numberAndStringSorter (arr) {
- 
+  const stringArr = []
+  const numbArr = []
+  arr.map(item => typeof item === 'string' ? stringArr.push(item) : numbArr.push(item))
+  const numbArrLength = numbArr.length
+  let k = 1
+  for (let i = 0; i < numbArrLength; i++) {
+    numbArr.splice((i + k), 0, stringArr[i])
+    k++
+  }
+  return numbArr
 }
 
 export default numberAndStringSorter
